b47a4f551c390dcfa0944b1a5907168516cd4897
nvsblmike <soundboylesh77@gmail.com>
2022-03-17 06:04:31 -0700

    wrgwgww

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,47 +1,17 @@
 #include "main.h"
-
-
-
 /**
-
- * main - check the code for School students
-
- *
-
- * Program that prints _putchar, followed by a new line
-
- *
-
- * Return: always 0 (success)
-
- */
-
-
-
+* Main - Entry point
+* Return - Always 0(Success)
+*/
 int main(void)
-  
 {
-  
-  char s[] = "_putchar";
-  
   int i = 0;
-  
-
-  
+  char s[] = "_putchar";
   while (s[i] != '\0')
-    
     {
-      
       _putchar(s[i]);
-      
       i++;
-      
     }
-  
   _putchar(10);
-  
-
-  
-  return (0);
-  
+  return 0;
 }
