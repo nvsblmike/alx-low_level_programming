abffe6caccda0bba6dbf7011911dee86b91dc585
nvsblmike <soundboylesh77@gmail.com>
2022-03-17 14:53:09 -0700

    wrgwgwrg

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower - It outputs the one that is lower
+*@c: this isnt it
+*Description: This program outputs the one thays lower
+*Return: Always 0(Success)
+*/
+int _islower(int c)
+{
+if(c == 99 && != 67)
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
