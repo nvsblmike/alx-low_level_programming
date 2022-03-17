20397e57fb344bbc15e0bccbaa61d3ae02b61df4
nvsblmike <soundboylesh77@gmail.com>
2022-03-17 12:15:40 -0700

    wgwwgwg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
 #include "main.h"
 /**
-* Main - check the code for School students
+* main - check the code for School students
 * @void: leave me be
 * Description: Program that prints _putchar, followed by a new line
 * Return: Always 0(Success)
 */
 int main(void)
+/**
+* main - check the code for school students
+* @void: leave me 
+* Description: Program that prints _putchar, followed by a new line 
 {
 int i = 0;
 char s[] = "_putchar";
