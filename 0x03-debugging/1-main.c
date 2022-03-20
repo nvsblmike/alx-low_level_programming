8688b18a78581e596d9b5a685dd6a1b23e65f4ff
nvsblmike <soundboylesh77@gmail.com>
2022-03-20 15:15:28 -0700

    afafa

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
 * main - causes an infinite loop
+* @void: gigighksdkjdg
+* Description: wdgfuhgkggkjgghgfk
 * Return: 0
 */
 int main(void)
 {
 int i;
-printf("Infinite loop incoming :(\n");
-/**
+printf("Infinite loop incoming :(\n");/**
 *i = 0;
 *while (i < 10)
 *{
