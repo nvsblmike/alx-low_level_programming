c23d50714f325df02b6fb88c8360e5c06f5e0b12
nvsblmike <soundboylesh77@gmail.com>
2022-03-22 04:44:37 -0700

    afafa

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
-*
-*
-*
-*
+*_strlen - sdfjfjfna
+*Description: sjvjnwjnfjw
+*@s:swjdjfwfjwnfjfnw
 */
 int _strlen(char *s)
 {
-printf("%d\n", sizeof(s));
+_putchar("%d\n", sizeof(s));
 }
