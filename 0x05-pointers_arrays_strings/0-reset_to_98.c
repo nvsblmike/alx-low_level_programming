54862db276cb6de894efac179045f4e73c4c4122
nvsblmike <soundboylesh77@gmail.com>
2022-03-22 04:05:35 -0700

    wfnwfjwnf

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,9 @@
 #include "main.h"
 /**
-* main - check the code
+* reset_to_98 - wewjjffbwfjwbhf
 * Description: qwetyyhg
-* @void: sfjgfjsgjs
+* @*n: sfjgfjsgjs
 * Return: Always 0
 */
-int main(void)
-{
-int n;
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
-}
+void reset_to_98(int *n)
+*n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/**
-* reset_to_98 - wwjgwigiuwf
-* @*n: iwfiwfwuifwf
-* Description: egjjnegejegnekgjg
-*/
-void reset_to_98(int *n)
-{
-*n = 98;
-}
+void reset_to_98(int *n);
