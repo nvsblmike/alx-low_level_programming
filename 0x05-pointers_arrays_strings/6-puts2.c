00dd329b1fe693163872293560b717da013e4a3a
nvsblmike <soundboylesh77@gmail.com>
2022-03-23 11:59:24 -0700

    tt

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*puts2 - edrtgyhuj
+*Description: etrgtyhuu
+*@str: yrdtfygouiygfut
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i = i + 2;
+}
+_putchar('\n');
+}
