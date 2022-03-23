885e6345445d0f0edb258abd4a5370ba9348e554
nvsblmike <soundboylesh77@gmail.com>
2022-03-23 12:06:49 -0700

    qeerqrq

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,16 @@
 */
 void puts2(char *str)
 {
+int j, len;
 int i = 0;
 while (str[i] != '\0')
 {
-_putchar(str[i]);
-i = i + 2;
+i++;
+}
+len = i;
+for (j = 0; j < len; j +=2)
+{
+_putchar(str[j]);
 }
 _putchar('\n');
 }
