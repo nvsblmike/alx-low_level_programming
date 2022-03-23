ad0a77252aba7dff43a1f5943cd931ff3859fc2c
nvsblmike <soundboylesh77@gmail.com>
2022-03-23 14:51:34 -0700

    sddfsgsg

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_array - qwertyui
+*Description: FUnction that prinnts elements in an array
+*@n: parameter
+*@a: parameter
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d\n",a[i]);
+if (i != (n - 1))
+{
+printf(", ");
+}
+}
+printf("\n");
+}
