44139159e9f3376935ee99edf7590faf756110c5
nvsblmike <soundboylesh77@gmail.com>
2022-03-23 14:17:55 -0700

    adadfdfa

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*puts_half - the function
+*Description: It prints the second half of a string 
+*@str: parameter that receives passed argument
+*/
+void puts_half(char *str)
+{
+int a, b, len, startindex, endindex;
+a = 0;
+while (str[a] != '\0')
+{
+a++;
+}
+len = a;
+endindex = len - 1;
+startindex = (endindex / 2) + 1;
+for (j = startindex; j <  len; j++)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
