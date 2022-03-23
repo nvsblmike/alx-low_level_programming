f5a2192b608d6c22626d8254692745305622be8e
nvsblmike <soundboylesh77@gmail.com>
2022-03-23 11:44:11 -0700

    ert

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*rev_string - ertyuio
+*Description: edrftgyhuik
+*@s: dfghj
+*/
+void rev_string(char *s)
+{
+int a, b, len, temp, lastindex;
+a = 0;
+while (s[a] != '\0')
+{
+a++;
+}
+len = a;
+lastindex = len - 1;
+for (b = 0; b < len/2; b++)
+{
+temp = s[b];
+s[b] = s[lastindex];
+s[lastindex--] = temp;
+}
+}
