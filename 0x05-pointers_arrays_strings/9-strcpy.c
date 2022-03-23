68622fb084d685bce0b769a4de6215af88e31106
nvsblmike <soundboylesh77@gmail.com>
2022-03-23 15:12:57 -0700

    sgsgsgs

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+**_strcpy - a function
+*Descripion: This function copies a string from a source and sends it to a destination
+* @dest: one of the parameters
+*@src: the other parameters
+*Return: Always dest(Success)
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i, j, len;
+i = 0;
+while (src[i] != '\0')
+{
+i++;
+}
+len = i;
+for (j = 0; j < len; j++)
+{
+dest[j] = src[j];
+}
+dest[j] = '\0';
+return (dest);
+}
