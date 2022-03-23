090d22d943c86c4cff1733b3fe343c8f336972ea
nvsblmike <soundboylesh77@gmail.com>
2022-03-23 11:01:43 -0700

    afdff

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,8 +9,8 @@ void _puts(char *str)
 int a;
 while (str[a] != '\0')
 {
-_puts(str[a]);
+_putchar(str[a]);
 a++;
 }
-_puts('\n');
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@ void print_rev(char *s)
 {
 int i, j, len;
 i = 0;
-while(s[i] != '\0')
+while (s[i] != '\0')
 {
 i++;
 }
 len = i;
 j = len - 1;
-while(j >= 0)
+while (j >= 0)
 {
 _putchar(s[j]);
 j--;
