f03b4e637210ce771a9468d5cc3e2cdb272291e4
nvsblmike <soundboylesh77@gmail.com>
2022-03-25 16:14:38 -0700

    sfgssg

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers - the function
+*Description: It prints the numbers 0 to 9
+*@void: doesn't take no parameter
+*/
+void print_numbers(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+_putchar(a);
+}
+}
