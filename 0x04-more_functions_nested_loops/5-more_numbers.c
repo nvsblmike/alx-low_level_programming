ded89cf22f97c7620a70bc79e0ea70d65df7a7ce
nvsblmike <soundboylesh77@gmail.com>
2022-03-28 11:31:52 -0700

    wswsdgsgsdg

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*more_numbers - this is a function
+*Description: This prints 0-14 10 times
+*@void: no argument is passed
+*/
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+_putchar(j + '0');
+}
+_putchar('\n');
+}
+}
