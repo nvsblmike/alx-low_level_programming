f076ccc0acd5d7b3ab5d53064f44ed4195b48746
nvsblmike <soundboylesh77@gmail.com>
2022-03-30 00:28:46 -0700

    asdffdadf

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,24 +13,24 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
-putchar(' ');
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-putchar(' ');
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-putchar(' ');
 }
 else
 {
 printf("%d", i);
-putchar(' ');
+}
+if (i < 100)
+{
+printf(" ");
 }
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
