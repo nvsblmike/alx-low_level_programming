1a8b96a0c4bfce59912fb35f400c337c061eb3c0
nvsblmike <soundboylesh77@gmail.com>
2022-04-04 11:57:06 -0700

    add file 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _memset - this is the function
+* Description: it fills in the array space allocated to an array with sth
+* @s: parameter
+* @b: parameter
+* @n: parameter
+* Return: Nothing
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for(i = 0; i < n; i++){
+s[i] = b;
+} 
+_putchar(s[i] + '0'); 
+_putchar('\n');
+}
