aae8e9157e7bf1a9cb32ffb4c89f39e706b95624
nvsblmike <soundboylesh77@gmail.com>
2022-04-07 16:10:15 -0700

    lets look at this

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,14 +10,14 @@ char *_strdup(char *str)
 char *m;
 unsigned int i;
 m = malloc(sizeof(char) *sizeof(*str));
-if (str[i] == 0 || m == NULL)
-{
-return (NULL);
-}
 for (i = 0; i < sizeof(*str); i++)
 {
 m[i] = str[i];
 }
 return (m);
+if (str[i] == 0 || m == NULL)
+{
+return (NULL);
+}
 free(m);
 }
