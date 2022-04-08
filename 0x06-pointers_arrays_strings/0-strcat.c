c4d14a0c501caa53aa27f91a4eae0a86609c3db4
nvsblmike <soundboylesh77@gmail.com>
2022-04-08 12:07:31 -0700

    concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strcat - the function
+* Description: it attaches src to dest
+* @dest: parameter
+* @src: parameter
+* Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+char *p;
+int i = 0, int k = 0, int j = 0, int len = 0;
+p = dest;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[k] != '\0')
+{
+k++;
+}
+for (len = i, j = 0; len < (i + k), j < k; len++, j++)
+{
+p[len] = src[j];
+}
+return dest;
+}
