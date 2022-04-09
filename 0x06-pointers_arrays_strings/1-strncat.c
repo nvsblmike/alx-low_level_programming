2758a007210860cca332f741ad72eb3f01c5d061
nvsblmike <soundboylesh77@gmail.com>
2022-04-09 04:13:21 -0700

    concat pt 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strncat - the function
+* Description: it adds src to dest until src[n]
+* @dest: parameter
+* @src: parameter
+* @n: parameter
+* Return: pointer
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+char *p;
+int i = 0;
+int j, len;
+p = dest;
+while (dest[i] != '\0')
+{
+i++;
+}
+for (len = i, j = 0; j <= n; len++, j++)
+{
+p[len] = src[j];
+}
+return (p);
+}
