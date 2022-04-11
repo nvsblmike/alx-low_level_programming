7878eac280bfa214b730c2159d3df83b8674940f
nvsblmike <soundboylesh77@gmail.com>
2022-04-11 12:48:44 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* cap_string - the function
+* Description: it capitalizes words
+* @a: parameter
+* Return: pointer
+*/
+char *cap_string(char *a)
+{
+int i;
+for (i = 0; a[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if (a[i] >= 'a'&& a[i] <= 'z')
+{
+a[i] = a[i] - 32;
+continue;
+}
+if (a[i] == ' ')
+{
+i++;
+if (a[i] >= 'a'&& a[i] <= 'z')
+{
+a[i] = a[i] - 32;
+continue;
+}
+}
+else
+{
+if (a[i] >= 'A'&& a[i] <= 'Z')
+{
+a[i] = a[i] + 32;
+}
+}
+}
+}
+return (a);
+}
