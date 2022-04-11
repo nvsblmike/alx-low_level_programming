c312b074f133787bb9cca0fa5a2a27ab5cd881dc
nvsblmike <soundboylesh77@gmail.com>
2022-04-11 06:01:27 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,11 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-char *p;
 int i = 0;
 int j;
-p = dest;
 for (i = 0, j = 0; j < n; i++, j++)
 {
-p[i] = src[j];
+dest[i] = src[j];
 }
-return (p);
+return (dest);
 }
