92057dafd838b0989f87ab0face6f71ae9be2140
nvsblmike <soundboylesh77@gmail.com>
2022-04-12 15:26:47 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,28 +10,11 @@ char *cap_string(char *a)
 int i;
 for (i = 0; a[i] != '\0'; i++)
 {
-if (i == 0)
-{
 if (a[i] >= 'a' && a[i] <= 'z')
 {
-a[i] = a[i] - 32;
-continue;
-}
-if (a[i] == ' ' || a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}' )
-{
-++i;
-if (a[i] >= 'a' && a[i] <= 'z')
+if ((a[i - 1] < 'A' && a[i - 1] > 'Z') || a[i - 1] != '-')
 {
 a[i] = a[i] - 32;
-continue;
-}
-}
-else
-{
-if (a[i] >= 'A' && a[i] <= 'Z')
-{
-a[i] = a[i] + 32;
-}
 }
 }
 }
