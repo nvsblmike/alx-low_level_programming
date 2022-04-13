d77554332e874b11b4a0baea5cc339dd1b8b0ef9
nvsblmike <soundboylesh77@gmail.com>
2022-04-13 13:31:04 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main (void)
+/**
+* main - the function
+* Description: it prints out the name of the compiler
+* @void: parameter
+* Return: (Success)
+*/
+int main(void)
 {
 char *p;
 p = __FILE__;
-printf("%s\n", p);
+printf("%s\n", *p);
+return (0);
 }
