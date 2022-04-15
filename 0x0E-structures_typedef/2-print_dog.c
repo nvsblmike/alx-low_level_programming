9632704dada7fd69111427ef8627b1fa7a25ba72
nvsblmike <soundboylesh77@gmail.com>
2022-04-14 23:38:29 -0700

    rint dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,35 @@
 */
 void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
+if (d->name == NULL)
+{
+printf("Name: (nil)");
+}
+else
+{
 printf("Name: %s\n", d->name);
+}
+if (d->age == NULL)
+{
+printf("Age: (nil)");
+}
+else
+{
 printf("Age: %f\n", d->age);
+}
+if (d->owner == NULL)
+{
+printf("Owner: (nil)");
+}
+else
+{
 printf("Owner: %s\n", d->owner);
+}
+}
+else if (d == NULL)
+{
+printf("");
+}
 }
