9b907acfb970071749f2272debed14591acfd548
nvsblmike <soundboylesh77@gmail.com>
2022-04-16 03:47:37 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-fwrite('\n');
+fprintf(stderr, "and that piece of art is useful\" - Dora");
+fprintf(stderr, " Korpar, 2015-10-19\n");
 return (1);
 }
