233653e3bbf17d1fef5e181ec0a12c621e5fc11f
nvsblmike <soundboylesh77@gmail.com>
2022-04-16 03:24:14 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* 
-* Return - Always 1 (Success)
+* Return: Always 1 (Success)
 */
 int main(void)
 {
-fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
