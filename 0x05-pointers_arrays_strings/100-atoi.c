1335023f281326f69455147387beec6c6b6bdc06
nvsblmike <soundboylesh77@gmail.com>
2022-04-28 14:41:42 -0700

    strings to ints

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ int _atoi(char *s)
 	int i, n;
 	n = 0;
 
-	for (i = 0; *(s + i) >= '0' && *(s + i) <= '9'; i++)
+	while (s[i] != '\0')
 	{
 		if (*(s + i) >= '0' && s[i] <= '9')
 		{
@@ -21,7 +21,7 @@ int _atoi(char *s)
 		}
 		else if (s[i] == '-')
 		{
-			n = 10 * n + ((-1) * s[i + 1] - '0');
+			n = 10 * n + (-1)*(s[i] - '0');
 		}
 		else{
 			return (0);
