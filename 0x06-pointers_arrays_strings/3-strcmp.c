d025584bd4adc22fe51d54b8679a9c5f90ca9689
nvsblmike <soundboylesh77@gmail.com>
2022-05-10 07:42:07 -0700

    chabges to strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,16 @@
 #include "main.h"
 /**
+ * _strcmp - the function
+ * Description: it comparrs two strings
  *
- *
- *
- *
- *
- *
+ * @s1: parameter
+ * @s2: parameter
+ * Return: s1
  */
+int *_strcmp(char s1, char s2)
+{
+	for ( ; *s1 == *s2; s1++, s2++)
+		if (*s1 == '\0')
+			return (0);
+	return (*s1 - *s2);
+}
