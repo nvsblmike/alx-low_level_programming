ca69b177764d7035590213a5ca0e80c300880487
nvsblmike <soundboylesh77@gmail.com>
2022-06-09 04:39:21 -0700

    strncpy edit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0, dest[i] = src[i] && i < n; i++)
+for (i = 0; dest[i] = src[i] && i < n; i++)
 ;
 *(dest + i) = '\0';
 return (dest);
