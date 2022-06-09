ef0987cee4bc8f433acae6446ae8a0f50e814b97
nvsblmike <soundboylesh77@gmail.com>
2022-06-09 05:54:07 -0700

    adjusted 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,6 +16,8 @@ char *cap_string(char *s)
 	}
 	for (i = 0; i < len; i++)
 	{
+		if (s[i - 1] == "" &&s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 		if (s[i - 1] >= ' ' && s[i - 1] <= '/' && s[i]  >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
 		else if (s[i - 1] >= ':' && s[i - 1] <= '@' && s[i]  >= 'a' && s[i] <= 'z')
