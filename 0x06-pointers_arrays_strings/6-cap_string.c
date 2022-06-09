d985327bf086ac7aa6ac616821bfe5ab11719fd2
nvsblmike <soundboylesh77@gmail.com>
2022-06-09 06:04:54 -0700

    adjustments

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,12 +9,9 @@ char *cap_string(char *s)
 {
 	int i, len;
 
-	len = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+	for (i = 1; s[i] != '\0'; i++)
 	{
 		if (s[i - 1] == "" &&s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
