f35ed715e3d53edb34b84ae3899dffa3957bef52
nvsblmike <soundboylesh77@gmail.com>
2022-06-20 04:58:04 -0700

    create file 5-strstr.c:

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,6 +8,7 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
+	int j = 0;
 	int i;
 
 	for (i = 0; haystack[i] != '\0'; i++)
@@ -15,5 +16,7 @@ char *_strstr(char *haystack, char *needle)
 		if (needle[0] == haystack[i] && needle[1] == haystack[i + 1])
 			return (&haystack[i]);
 	}
+	if (needle[0] == '\0')
+		return (&haystack[j]);
 	return ('\0');
 }
