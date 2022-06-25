1c806f60f51dd713833fc877f99044b5e63ec70a
nvsblmike <soundboylesh77@gmail.com>
2022-06-25 04:43:34 -0700

    created 0-puts_recursion.c file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _puts_recursion - function
+* Description: it prints out a string and nl
+* @s: parameter
+* void
+*/
+void _puts_recursion(char *s)
+{
+	_putchar('s');
+	_putchar('\n');
+}
