aa22d37df6fdc35de6eed7a22f308ef4bae1b813
nvsblmike <soundboylesh77@gmail.com>
2022-06-27 07:45:47 -0700

    created a file that prints length of str

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strlen_recursion - the function
+* Description: it gets the length of a string
+* @s: parameter
+* Return: integer
+*/
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	int n = 0;
+
+	if (s[i] == '\0')
+		return (n);
+	else
+		n++;
+	i++;
+	int _strlen_recursion(s++);
+}
