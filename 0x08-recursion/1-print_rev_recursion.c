35d22a836e84cd994cde33def48a569239d32a57
nvsblmike <soundboylesh77@gmail.com>
2022-07-21 11:36:54 -0700

    print rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,20 @@
 */
 void _print_rev_recursion(char *s)
 {
-	int i, len = 0;
-	char *p;
+	int c, t, j, len;
 
-	while (s[i] != '\0')
-		len++;
-	for (i = 0, len = len; p[i] = s[len]; i++, len--)
-		_putchar(p[i]);
+	t = 0;
+	while (s[t] != '\0')
+		t++;
+	len = t;
+	i = 0;
+	j = len - (i + 1);
+	if (i < j)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+	i = 1;
+	_print_rev_recursion(s++);
 }
