f897fccbe03bee7323c8bcaa7c921bcf286705bf
nvsblmike <soundboylesh77@gmail.com>
2022-09-14 05:36:22 -0700

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - the function
+* Description: it uses printf to output a string
+* @void: no parameter
+* Return: always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+}
