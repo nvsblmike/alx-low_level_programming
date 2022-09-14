435a2b832083e5af747444fe9f7cc72cd462852b
nvsblmike <soundboylesh77@gmail.com>
2022-09-14 05:30:18 -0700

    print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - the main function
+* Description: it prints out something
+* @void: no parameter
+* Return: always 0
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle);
+
+	return (0);
+}
