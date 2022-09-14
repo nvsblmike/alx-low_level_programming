237dca0fcf34181fdd5bd77c5d36c11e263a6779
nvsblmike <soundboylesh77@gmail.com>
2022-09-14 16:52:59 -0700

    print out a string without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,11 @@
 * main - function
 * Description: it prints a string
 * @void: no parameter
-* Return: always 0 (success)
+* Return: always 1 (success)
 */
 
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
