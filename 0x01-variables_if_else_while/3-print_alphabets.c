22bef9665267396ffe2915b75472148248fb7e27
nvsblmike <soundboylesh77@gmail.com>
2022-09-16 02:22:50 -0700

    print d alphabets both lower and upper cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - function
+* Description: prints alphabets
+* @void: no parameter
+* Return: always 0
+*/
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	c = 'A';
+
+	while (c >= 'a' && c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d >= 'A' && d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
