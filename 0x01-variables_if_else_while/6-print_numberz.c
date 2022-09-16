5eff44add031e6c6c293650c931a30d56026bf4b
nvsblmike <soundboylesh77@gmail.com>
2022-09-16 02:59:29 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - functuon
+* Description: print 0-9 but don't use cjar
+* @void: no parameter
+* Return: always 0
+*/
+
+int main(void)
+{
+	int c;
+
+	c = 48;
+
+	while (c >= 48 && c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
