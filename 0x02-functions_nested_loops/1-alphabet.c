db181fcc2d60c0f3dba5860c3f78ad448d01fe9f
nvsblmike <soundboylesh77@gmail.com>
2022-09-19 03:18:25 -0700

    print out alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet - function
+* Description: prints alphabet in lower case
+* @void: no parameter
+* Return: always 0
+*/
+
+void print_alphabet(void)
+{
+	int a = 'a';
+	int b = 'z';
+	int i = '\n';
+	int c = 'a';
+
+	while (c >= 'a' && c <= 'b')
+	{
+		_putchar(c);
+	}
+	_putchar(i);
+	return (0);
+}
