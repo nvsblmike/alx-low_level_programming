fd805421289e78aaccd71d4cf344288fb06e6735
nvsblmike <soundboylesh77@gmail.com>
2022-09-22 02:34:46 -0700

    find absolutr vakue

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _abs - function
+* Description: it removes signs
+* @n: parameter
+* Return: always sth
+*/
+
+int _abs(int n)
+{
+	int r;
+	int v;
+	r = 0;
+	v = -1;
+
+	if (n < 0)
+	{
+		r = n * v;
+		return (r);
+	}
+	else
+		return (n);
+}
