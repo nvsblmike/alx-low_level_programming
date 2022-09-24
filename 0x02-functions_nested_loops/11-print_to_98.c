f0f58ae4a96f0df504121917686c16956e150287
nvsblmike <soundboylesh77@gmail.com>
2022-09-24 08:12:04 -0700

    pront frim n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_to_98 - function
+* Description: it prints from n to 98
+* @void: no parameter
+* Return: always nth
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 99; i++)
+	{
+		_putchar(i);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 int add(int a, int b);
+void print_to_98(int n);
