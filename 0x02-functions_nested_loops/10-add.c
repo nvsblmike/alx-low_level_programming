7a8acb2ad10d9ac966738e28ee31524b60f2ca0e
nvsblmike <soundboylesh77@gmail.com>
2022-09-24 08:06:09 -0700

    adds two ints together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* add - function
+* Description: it adds two ints
+* @a: parameyer
+* @b: parameter
+* Return: always sth
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
